02b37d4195c410a9082b04b3d8ae39c9ef8cd758
Gerardo Fumero <gerardo.fumero@utp.ac.pa>
2021-12-13 20:53:16 -0500

    Cambios exitosos

--- Calculadora.py
+++ Calculadora.py
@@ -25,24 +25,24 @@ tokens = (
 #Se crean las funciones que permiten agregarle los valores a los tokens anteriores, tranformando los strings de entradas a cadenas.
 
 #Función para números decimales
-def t_FLOAT(p):
+def t_FLOAT(t):
     r'\d+\.\d+' #permite la entrada de numeros (d+) seguido de un punto y numeros que representan decimales
     try:
-        p.value = float(p.value)    #Permite medir el error según la diferencia en la variación del dato con respecto a su longitud
+        t.value = float(t.value)    #Permite medir el error según la diferencia en la variación del dato con respecto a su longitud
     except ValueError:
         print("El numero en decimales introducido es demasiado largo")
-        p.value = 0
-    return p
+        t.value = 0
+    return t
 
 #Función para números enteros
-def t_INT(p):
+def t_INT(t):
     r'\d+' #Permite la entrada sólo de números enteros sin decimales
     try: 
-        p.value = int(p.value)
+        t.value = int(t.value)
     except ValueError:
         print("El valor entero introducido es demasiado largo")
-        p.value = 0
-    return p
+        t.value = 0
+    return t
 
 
 
@@ -60,17 +60,17 @@ t_LEFTBRA = r'\['
 t_FACTORIAL = r'\!'
 
 #usaremos esto para ignorar espacios entre los valores de la expresión
-t_ignore = " \p"
+t_ignore = " \t"
 
 #funcion para señalar errores en la entrada por caracteres que no correspondan al lenguaje
-def t_error(p):
+def t_error(t):
     print("¡Syntax error!, el valor introducido no es permitido")
-    print("Valor prohibido: '%s'" % p.value[0])
-    p.lexer.skip(1)
+    print("Valor prohibido: '%s'" % t.value[0])
+    t.lexer.skip(1)
     
-def t_newline(p):
+def t_newline(t):
     r'\n+'
-    p.lexer.lineno += p.value.count("\n")
+    t.lexer.lineno += t.value.count("\n")
 
 ##Lexer como analizador léxico
 lexer = lex.lex()
@@ -89,19 +89,19 @@ precedence = (
 
 
 #Para el calculo puede haber una expresion o puede estar vacio
-def p_calculate(p):
+def p_calculat(t):
     '''
     expressions  : expression expressions
                 | expression 
                 | empty
     '''
 
-def p_solution(p):
-    'expression : CALCULATE RIGHTBRA expression LEFTBRA FIN'
-    print('Resultado: ' + str(p[3]))
+def p_solutio(t):
+    'expression : CALCULATE LEFTBRA expression RIGHTBRA FIN'
+    print('Resultado: ' + str(t[3]))
 
 #Se definen las operaciones 
-def p_expression_solutions(p):
+def p_expression_solution(t):
     '''
     expression  : expression MULTIPLY expression
                 | expression DIVIDE expression 
@@ -109,45 +109,45 @@ def p_expression_solutions(p):
                 | expression MINUS expression 
                 | FACTORIAL expression
     '''
-    if p[2] == '+': p[0] = p[1] + p[3]
-    elif p[2] == '-': p[0] = p[1] - p[3]
-    elif p[2] == '*': p[0] = p[1] * p[3]
-    elif p[2] == '/': p[0] = p[1] / p[3]
-    elif p[1] == '!':  p[0] =  factorial(p[2])
+    if t[2] == '+': t[0] = t[1] + t[3]
+    elif t[2] == '-': t[0] = t[1] - t[3]
+    elif t[2] == '*': t[0] = t[1] * t[3]
+    elif t[2] == '/': t[0] = t[1] / t[3]
+    elif t[1] == '!':  t[0] =  factorial(t[2])
 
 #Las expresiones puede ser de tipo integer o float
-def p_expressions_int_float(p):
+def p_expressions_int_floa(t):
     '''
     expression  : INT
                 | FLOAT 
     '''
-    p[0]=p[1]
+    t[0]=t[1]
     
 #Expresiones para números negativos
-def p_negative(p):
+def p_negativ(t):
     'expression : MINUS expression %prec UMINUS'
-    p[0] = -p[2]
+    t[0] = -t[2]
 
 #Se definen las expresiones con los paréntesis
-def p_expressions_parenthesis(p):
+def p_expressions_parenthesi(t):
     'expression : LEFTPAR expression RIGHTPAR'
-    p[0] = p[2]
+    t[0] = t[2]
 
-#se define p[0] como vacio para guardar un resultado
-def p_empty(p):
+#se define t[0] como vacio para guardar un resultado
+def p_empt(t):
     '''
     empty : 
     '''
-    p[0] = None 
+    t[0] = None 
 
 #Si se llega a encontrar un error en las expresiones
-def p_error(p):
+def p_error(t):
     print("ERROR: Fallo en la entrada, operación no permitida")
 
 parser = yacc.yacc()
 
-p = open("./valores.txt", "r")
-input = p.read()
+t = open("./valores.txt", "r")
+input = t.read()
 print(input)
 parser.parse(input)
 
--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'leftPLUSMINUSleftFACTORIALPLUSDIVIDErightUMINUSCALCULATE DIVIDE FACTORIAL FIN FLOAT INT LEFTBRA LEFTPAR MINUS MULTIPLY PLUS RIGHTBRA RIGHTPAR\n    expressions  : expression expressions\n                | expression \n                | empty\n    expression : CALCULATE RIGHTBRA expression LEFTBRA FIN\n    expression  : expression MULTIPLY expression\n                | expression DIVIDE expression \n                | expression PLUS expression\n                | expression MINUS expression \n                | FACTORIAL expression\n    \n    expression  : INT\n                | FLOAT \n    expression : MINUS expression %prec UMINUSexpression : LEFTPAR expression RIGHTPAR\n    empty : \n    '
+_lr_signature = 'leftPLUSMINUSleftFACTORIALPLUSDIVIDErightUMINUSCALCULATE DIVIDE FACTORIAL FIN FLOAT INT LEFTBRA LEFTPAR MINUS MULTIPLY PLUS RIGHTBRA RIGHTPAR\n    expressions  : expression expressions\n                | expression \n                | empty\n    expression : CALCULATE LEFTBRA expression RIGHTBRA FIN\n    expression  : expression MULTIPLY expression\n                | expression DIVIDE expression \n                | expression PLUS expression\n                | expression MINUS expression \n                | FACTORIAL expression\n    \n    expression  : INT\n                | FLOAT \n    expression : MINUS expression %prec UMINUSexpression : LEFTPAR expression RIGHTPAR\n    empty : \n    '
     
-_lr_action_items = {'CALCULATE':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,27,28,],[4,4,4,4,-10,-11,4,4,4,4,4,4,-12,-9,-5,-6,-7,-8,4,-13,-8,-4,]),'FACTORIAL':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,27,28,],[6,6,6,6,-10,-11,6,6,6,6,6,6,-12,-9,-5,-6,-7,-8,6,-13,-8,-4,]),'INT':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,27,28,],[7,7,7,7,-10,-11,7,7,7,7,7,7,-12,-9,-5,-6,-7,-8,7,-13,-8,-4,]),'FLOAT':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,27,28,],[8,8,8,8,-10,-11,8,8,8,8,8,8,-12,-9,-5,-6,-7,-8,8,-13,-8,-4,]),'MINUS':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,],[5,14,5,5,-10,-11,5,5,5,5,5,5,-12,-9,24,24,-6,-7,-8,24,5,-13,-8,-4,]),'LEFTPAR':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,27,28,],[9,9,9,9,-10,-11,9,9,9,9,9,9,-12,-9,-5,-6,-7,-8,9,-13,-8,-4,]),'$end':([0,1,2,3,7,8,10,16,17,19,20,21,22,25,27,28,],[-14,0,-2,-3,-10,-11,-1,-12,-9,-5,-6,-7,-8,-13,-8,-4,]),'MULTIPLY':([2,7,8,16,17,18,19,20,21,22,23,25,27,28,],[11,-10,-11,-12,-9,11,11,-6,-7,-8,11,-13,-8,-4,]),'DIVIDE':([2,7,8,16,17,18,19,20,21,22,23,25,27,28,],[12,-10,-11,-12,-9,12,12,-6,12,12,12,-13,12,-4,]),'PLUS':([2,7,8,16,17,18,19,20,21,22,23,25,27,28,],[13,-10,-11,-12,-9,13,13,-6,-7,-8,13,-13,-8,-4,]),'RIGHTBRA':([4,],[15,]),'RIGHTPAR':([7,8,16,17,18,19,20,21,25,27,28,],[-10,-11,-12,-9,25,-5,-6,-7,-13,-8,-4,]),'LEFTBRA':([7,8,16,17,19,20,21,23,25,27,28,],[-10,-11,-12,-9,-5,-6,-7,26,-13,-8,-4,]),'FIN':([26,],[28,]),}
+_lr_action_items = {'CALCULATE':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,27,28,],[4,4,4,4,-10,-11,4,4,4,4,4,4,-12,-9,-5,-6,-7,-8,4,-13,-8,-4,]),'FACTORIAL':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,27,28,],[6,6,6,6,-10,-11,6,6,6,6,6,6,-12,-9,-5,-6,-7,-8,6,-13,-8,-4,]),'INT':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,27,28,],[7,7,7,7,-10,-11,7,7,7,7,7,7,-12,-9,-5,-6,-7,-8,7,-13,-8,-4,]),'FLOAT':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,27,28,],[8,8,8,8,-10,-11,8,8,8,8,8,8,-12,-9,-5,-6,-7,-8,8,-13,-8,-4,]),'MINUS':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,],[5,14,5,5,-10,-11,5,5,5,5,5,5,-12,-9,24,24,-6,-7,-8,24,5,-13,-8,-4,]),'LEFTPAR':([0,2,5,6,7,8,9,11,12,13,14,15,16,17,19,20,21,22,24,25,27,28,],[9,9,9,9,-10,-11,9,9,9,9,9,9,-12,-9,-5,-6,-7,-8,9,-13,-8,-4,]),'$end':([0,1,2,3,7,8,10,16,17,19,20,21,22,25,27,28,],[-14,0,-2,-3,-10,-11,-1,-12,-9,-5,-6,-7,-8,-13,-8,-4,]),'MULTIPLY':([2,7,8,16,17,18,19,20,21,22,23,25,27,28,],[11,-10,-11,-12,-9,11,11,-6,-7,-8,11,-13,-8,-4,]),'DIVIDE':([2,7,8,16,17,18,19,20,21,22,23,25,27,28,],[12,-10,-11,-12,-9,12,12,-6,12,12,12,-13,12,-4,]),'PLUS':([2,7,8,16,17,18,19,20,21,22,23,25,27,28,],[13,-10,-11,-12,-9,13,13,-6,-7,-8,13,-13,-8,-4,]),'LEFTBRA':([4,],[15,]),'RIGHTPAR':([7,8,16,17,18,19,20,21,25,27,28,],[-10,-11,-12,-9,25,-5,-6,-7,-13,-8,-4,]),'RIGHTBRA':([7,8,16,17,19,20,21,23,25,27,28,],[-10,-11,-12,-9,-5,-6,-7,26,-13,-8,-4,]),'FIN':([26,],[28,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -27,18 +27,18 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> expressions","S'",1,None,None,None),
-  ('expressions -> expression expressions','expressions',2,'p_calculate','Calculadora.py',98),
-  ('expressions -> expression','expressions',1,'p_calculate','Calculadora.py',99),
-  ('expressions -> empty','expressions',1,'p_calculate','Calculadora.py',100),
-  ('expression -> CALCULATE RIGHTBRA expression LEFTBRA FIN','expression',5,'p_solution','Calculadora.py',104),
-  ('expression -> expression MULTIPLY expression','expression',3,'p_expression_solutions','Calculadora.py',110),
-  ('expression -> expression DIVIDE expression','expression',3,'p_expression_solutions','Calculadora.py',111),
-  ('expression -> expression PLUS expression','expression',3,'p_expression_solutions','Calculadora.py',112),
-  ('expression -> expression MINUS expression','expression',3,'p_expression_solutions','Calculadora.py',113),
-  ('expression -> FACTORIAL expression','expression',2,'p_expression_solutions','Calculadora.py',114),
-  ('expression -> INT','expression',1,'p_expressions_int_float','Calculadora.py',125),
-  ('expression -> FLOAT','expression',1,'p_expressions_int_float','Calculadora.py',126),
-  ('expression -> MINUS expression','expression',2,'p_negative','Calculadora.py',132),
-  ('expression -> LEFTPAR expression RIGHTPAR','expression',3,'p_expressions_parenthesis','Calculadora.py',137),
-  ('empty -> <empty>','empty',0,'p_empty','Calculadora.py',143),
+  ('expressions -> expression expressions','expressions',2,'p_calculat','Calculadora.py',94),
+  ('expressions -> expression','expressions',1,'p_calculat','Calculadora.py',95),
+  ('expressions -> empty','expressions',1,'p_calculat','Calculadora.py',96),
+  ('expression -> CALCULATE LEFTBRA expression RIGHTBRA FIN','expression',5,'p_solutio','Calculadora.py',100),
+  ('expression -> expression MULTIPLY expression','expression',3,'p_expression_solution','Calculadora.py',106),
+  ('expression -> expression DIVIDE expression','expression',3,'p_expression_solution','Calculadora.py',107),
+  ('expression -> expression PLUS expression','expression',3,'p_expression_solution','Calculadora.py',108),
+  ('expression -> expression MINUS expression','expression',3,'p_expression_solution','Calculadora.py',109),
+  ('expression -> FACTORIAL expression','expression',2,'p_expression_solution','Calculadora.py',110),
+  ('expression -> INT','expression',1,'p_expressions_int_floa','Calculadora.py',121),
+  ('expression -> FLOAT','expression',1,'p_expressions_int_floa','Calculadora.py',122),
+  ('expression -> MINUS expression','expression',2,'p_negativ','Calculadora.py',128),
+  ('expression -> LEFTPAR expression RIGHTPAR','expression',3,'p_expressions_parenthesi','Calculadora.py',133),
+  ('empty -> <empty>','empty',0,'p_empt','Calculadora.py',139),
 ]
